54d927d8b42c733bb07a64aa832cdab5b2420a01
Smitty <stobor2004@gmail.com>
2016-11-09 15:45:43 -0500

    The site is responsive.

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,7 +1,29 @@
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
-  map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
     center: {lat: 40.7413549, lng: -73.9980244},
     zoom: 13
   });
+
+  // test: marker
+  // var tribeca = {lat: 40.719526, lng: -73.9980244};
+  // var marker = new google.maps.Marker({
+  //   position: tribeca,
+  //   map: map,
+  //   title: 'First Marker!'
+  // });
+  //
+  // var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 }
+
+// google.maps.event.addDomListener(window, 'load', initMap);
+
+// // code copied from https://github.com/twbs/bootstrap/issues/2475
+// function myFunction() {
+//   $(window).resize(function () {
+//       var h = $(window).height(),
+//           offsetTop = 60; // Calculate the top offset
+//
+//       $('#map-canvas').css('height', (h - offsetTop));
+//   }).resize();
+// }
