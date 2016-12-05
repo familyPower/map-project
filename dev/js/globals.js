d7bdf8e88fc7ee74195987c5189458f98d7f7b5f
Smitty <stobor2004@gmail.com>
2016-12-05 11:11:51 -0500

    Added comments to globals.js.

--- dev/js/globals.js
+++ dev/js/globals.js
@@ -3,14 +3,16 @@
  * purpose: Holds variable and functions that are available globally
  */
 
+// Indicates the current or desired state of the details row.
 var open = "OPEN";
 var close = "CLOSE";
 
 /**
  * isValidLatLong - description
  *
- * @param  {type} latLon description
- * @return {type}        description
+ * @param  {type} latLon GoogleMap latlon object. Holds latitude and longitude.
+ * @return {type}        True iff lat & lng are within range, else false.
+ * Purpose        Validates that lat & lng are within range.
  */
 function isValidLatLong(latLon) {
     var lat = latlon.lat;
@@ -41,25 +43,66 @@ function isEqualLatLng(loc1, loc2) {
   return loc1.lat() == loc2.lat() && loc1.lng() == loc2.lng();
 }
 
+
+/**
+ * assert - Throws error if condition is false, else it does nothing.
+ *
+ * @param  {type} condition Boolean statement to evaluate.
+ * @param  {type} message   Message to display if condition fails.
+ * @return {type}           none.
+ */
 function assert(condition, message) {
     if (!condition) {
         throw message || "Assertion failed";
     }
 }
 
+
+/**
+ * precondition - Throws error if condition is false, else it does nothing.
+ *
+ * @param  {type} condition Boolean statement to evaluate.
+ * @param  {type} message   Message to display if condition fails.
+ * @return {type}           None.
+ * <p>
+ * comment        Works the same as an assert, however the name indicates this
+ *                    function is used to ensure all conditions exist to
+ *                    permit a function to operate correctly.
+ * </p>
+ */
 function precondition(condition, message) {
     if (!condition) {
         throw message || "Precondition failed";
     }
 }
 
+/**
+ * postcondition - Ensures state is valid after a function executes.
+ *
+ * @param  {type} condition oolean statement to evaluate.
+ * @param  {type} message   Message to display if condition fails.
+ * @return {type}           None
+ */
 function postcondition(condition, message) {
     if (!condition) {
         throw message || "Postcondition failed";
     }
 }
 
+
+/**
+ * findMarkerInArrayByKey - Searches the array of Markers for a marker with
+ *    the passed in key.
+ *
+ * @param  {type} array The array of Markers to search. Marker object must
+ *                        contain the application specific field 'Key'.
+ * @param  {type} key   The key value to search for.
+ * @return {type}       The index of the found marker or -1 if not found.
+ *
+ */
 function findMarkerInArrayByKey(array, key) {
+  precondition(array.length > 0 && 'Key' in array[0]);
+
   var bFound = false;
 
   var iRv = -1; // not found
@@ -72,9 +115,26 @@ function findMarkerInArrayByKey(array, key) {
   return iRv;
 }
 
+/**
+ * findMarkerInArrayByMarker -  - Searches the array of Markers for a marker
+ *    that contains the same lat. lng. as the passed in marker. If two Markers
+ *    contain the same lat. lng., they are considered to point to the same
+ *    location.
+ *<p>
+ * The code currently assumes that the marker will contain application specific
+ *  data. This may not hold true in the future.
+ *
+ * @param  {type} array  The array of Markers to search
+ * @param  {type} marker The marker to find.
+ * @return {type}        The index of the found marker or -1 if not found.
+ */
 function findMarkerInArrayByMarker(array, marker) {
-  assert(array);
-  assert(marker);
+  precondition(array);
+  precondition(marker);
+
+  assert(array.length > 0);
+  assert ('Key' in array[0] && 'Key' in marker);
+
 
   var bFound = false;
 
@@ -89,6 +149,14 @@ function findMarkerInArrayByMarker(array, marker) {
   return iRv;
 }
 
+
+/**
+ * arrayContains - Search the array for value.
+ *
+ * @param  {type} array The array to search.
+ * @param  {type} value The item to search for.
+ * @return {type}       true iff the value is in the array, else false.
+ */
 function arrayContains(array, value) {
   if (!array || array.length <= 0){
     return false;
@@ -102,6 +170,14 @@ function arrayContains(array, value) {
 
   return false;
 }
+
+
+/**
+ * isValidState - Used to ensure that a valid state place detail panel.
+ *
+ * @param  {type} state The state used, as a parameter etc.
+ * @return {type}       true if state is valid, else false. 
+ */
 function isValidState(state) {
   return state == open || state == close;
 }
