15dfe0d2e0bfad153c9c0d941e9f89b6bf3b89d7
Smitty <stobor2004@gmail.com>
2016-12-08 18:05:12 -0500

    corrections after review.

--- dev/readme.md
+++ dev/readme.md
@@ -1,11 +1,52 @@
+How to use the program
+===============
+The application will start with the map center at the Empire State Building in
+New York City. There will be points of interest (POI) to the left of the map.
+There will also be articles from the New York Times and Wikipedia below the map.
+You can click on any of the articles to read the article. If you click on a POI,
+you will see any Wikipedia articles available about the POI clicked on.
+
+You can change the center of the map by typing in an address or a landmark. The
+map center will adjust to include the closest location to the what was typed in.
+The address of the center of the map will display directly above the map.
+
+The new links may be clicked to display additional information.
+
+Step by step instructions to run the application (assuming application is run locally)
+================================================
+1. Type localhost: 8000/dev
+The application is now running.
+
+To see a different location:
+1. Type the address or name of the landmark in the entry area.
+2. Press enter or click on the "Search" button.
+
+To see a POI:
+1. Click on marker for the POI or the name (located to the left of the map).
+
+To stop a POI marker from bouncing and to close the inforwindow, do one of the
+following:
+- Click on the same link clicked on to open the Infowindow
+or
+- click on the bouncing marker
+or
+- Click on the 'X' in the infowindow.
+
+To get detailed information about the POI
+- Click on the name of the POI
+or
+- Click on the marker for the POI.
+
+
 Project Capabilities:
+=====================
 This is a map project that allows users to type in an address, landmark or
 a location (i.e. Egypt, Pyramids; Eiffel Tower; Tower of London;
 3 Haven Plaza New York, 10009). Google Maps will search for the location. The
 address of the center of the map (the place Google Maps found) is displayed
 above the map. A list of POI (Points Of Interest) is displayed to the left of
 the map. Below the map are New York Times articles found about the address with
-Wikipeida articles to the left of the New York Times articles.
+Wikipedia articles to the left of the New York Times articles.
 
 If the user selects a place, an icon representing that location will bounce and
 an Infowindow will open displaying the name of the place. Bouncing can be
@@ -35,10 +76,41 @@ Note: If a location can't be found, the original location remains and the
 
 How to host the application locally.
 
-How to use Grunt:
+Task Runner:
+I use a simple task runner that only reduces the size of each file. Files are
+first minimized and then compressed to achieve significant size of transmitted
+files. All Application files are minimized and compressed.
+
+To execute gruntfile.js (From a Unix based system.):
   Install required dependencies/packages
+Copy and paste the following lines to install required dependencies. Copy each
+line execute each line separately.
+
+npm install grunt-contrib-uglify --save-dev
+npm install grunt-contrib-cssmin --save-dev
+npm install grunt-contrib-htmlmin --save-dev
+npm install grunt-contrib-compress --save-dev
+
   Run the build process.
-  Serve application
+Then execute grunt from the command line by typing in 'grunt'.
+
+Serve application
+The next step is to start a local webserver. I used python. Please refer to the
+documentation for the server you use.
+
+To use python, type:
+python -m SimpleHTTPServer xxxx (where 'xxxx' is the port you want to use, I
+used 8000).
+
+In your brower, type:
+localhost: xxxx (matches the port you chose) /dev|dist
+
+To execute the distribution version of the application, assuming port 8000 is
+chosen, type:
+localhost: 8000/dist
+
+Replace 'dist' with 'dev' to run the development (uncompressed and not minified)
+version of the code.
 
   ************************** Improve App ***************************
 
--- gruntfile.js
+++ gruntfile.js
@@ -1,177 +1,87 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    // Will delete files for `dist` target
-    // Will NOT delete files for `dev` target
-    clean: {
-      contents: ['dist'],
-      dev: {
-        options: {
-          'no-write': true
-        },
-        src: ['dev']
-      }
-    },
-
-    // copy folder structure and files
-    copy: {
-      main: {
-        files: [
-          // includes files within path
-          //{expand: true, src: ['path/*'], dest: 'dest/', filter: 'isFile'},
-
-          // includes files within path and its sub-directories
-          //{expand: true, src: ['dev/**'], dest: 'dist'},
-
-          // makes all src relative to cwd
-          {expand: true, cwd: 'dev/', src: ['**'], dest: 'dist/'},
-
-          // flattens results to a single level
-          //{expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'},
-        ],
-      },
-    },
-
-    // rename paths to css, js, files etc.
-    replace: {
-          dist: {
+        //minimize js
+        uglify: {
             options: {
-              patterns: [
-                {
-                  match: /\.css/g,
-                  replacement: '.min.css'
-                },
-                // {
-                //   match: /\.html/g,
-                //   replacement: '.min.html'
-                // },
-                {
-                  match: /[^(min)]\.js/g,
-                  replacement: '.min.js'
-                }
-              //   {
-              //     match: /(\.min){,2}/g,
-              //     replacement: '.min'
-              //   }
-             ]
+                // the banner is inserted at the top of the output
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
             },
-          files: [
-            {expand: true, flatten: false, src: ['dist/**/*.html']}
-          ]
-        }
-      },
-
-    //minimize js
-    uglify: {
-      options: {
-        // the banner is inserted at the top of the output
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          //'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-          'dist/js/script.min.js': ['dist/js/script.js']
-        }
-      }
-    },
-
-    // // minimize images
-    // imagemin: {
-    //    dist: {
-    //       options: {
-    //         optimizationLevel: 5
-    //       },
-    //       files: [{
-    //          expand: true,
-    //          cwd: 'dev/img',
-    //          src: ['**/*.{png,jpg,gif}'],
-    //          dest: 'dist/img'
-    //       },
-    //       {
-    //         expand: true,
-    //         cwd: 'dev/views/images',
-    //         src: ['**/*.{png,jpg,gif}'],
-    //         dest: 'dist/views/images'
-    //       }]
-    //    }
-    // },
-
-    htmlmin: {                                     // Task
-        dist: {                                      // Target
-          options: {                                 // Target options
-            removeComments: true,
-            collapseWhitespace: true
-          },
-          files: {                                   // Dictionary of files
-            'dist/index.html': 'dev/index.html'     // 'destination': 'source'
-          }
-        }
-      },
-
-    cssmin: {
-      minify: {
-        expand: true,
-      //cwd: 'css/',
-      // src: ['*.css', '!*.min.css', ],
-        files: {
-          'dist/css/style.min.css': ['dist/css/style.css']
-          // 'dist/css/print.min.css': ['dev/css/print.css'],
-          // 'dist/views/css/bootstrap-grid.min.css': ['dev/views/css/bootstrap-grid.css'],
-          // 'dist/views/css/style.min.css': ['dev/views/css/style.css']
-        }
-      // dest: 'dist/css',
-      // ext: '.min.css'
-      }
-    },
-
-    // gzip assets 1-to-1 for production
-    compress: {
-      main: {
-        options: {
-          mode: 'gzip'
+            my_target: {
+                files: [{
+                    expand: true,
+                    cwd: 'dev/js',
+                    src: '**/*.js',
+                    dest: 'dist/js'
+                        //'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                        //                    'dist/js/*.min.js': ['dev/js/*.js']
+                }]
+            }
         },
-        expand: true,
-        cwd: 'dist',
-        src: ['*.html', 'css/*.css','js/*.js'],
-        dest: 'dist'
-      }
-    }
 
-});
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-replace');
-  // grunt.loadNpmTasks('grunt-string-replace');
-  grunt.loadNpmTasks('grunt-contrib-compress');
+        htmlmin: { // Task
+            dist: { // Target
+                options: { // Target options
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: { // Dictionary of files
+                    'dist/index.html': 'dev/index.html' // 'destination': 'source'
+                }
+            }
+        },
 
-  // Default task(s).
-  grunt.registerTask('test', ['copy']);
-  grunt.registerTask('test2', ['clean','copy','replace']);
-  grunt.registerTask('test3', ['clean','copy','replace','uglify','cssmin']);
+        cssmin: {
+            minify: {
+                files: [{
+                    expand: true,
+                    cwd: 'dev/css',
+                    src: '**/*.css',
+                    dest: 'dist/css',
+                    // ext: '.min.css'
+                }]
+            }
+        },
 
-  //grunt.registerTask('all', ['uglify', 'imagemin', 'cssmin', 'htmlmin']);
+        // gzip assets 1-to-1 for production
+        compress: {
+            main: {
+                options: {
+                    mode: 'gzip'
+                },
+                expand: true,
+                cwd: 'dist',
+                src: ['*.html', 'css/*.css', 'js/*.js'],
+                dest: 'dist'
+                    //     }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-replace');
+    // grunt.loadNpmTasks('grunt-string-replace');
+    grunt.loadNpmTasks('grunt-contrib-compress');
 
-  grunt.registerTask('all', ['clean','copy','replace','uglify','imagemin','cssmin','htmlmin', 'compress']);
+    grunt.registerTask('default', ['uglify', 'cssmin', 'htmlmin', 'compress']);
+    // grunt.registerTask('default', ['uglify', 'cssmin', 'htmlmin', 'replace']);
 
-  // grunt.registerTask('default', ['clean','uglify','imagemin','cssmin','htmlmin','replace']);
-  grunt.registerTask('default', ['clean','copy','replace','uglify','cssmin','htmlmin']);
-
-  /*
-  npm install grunt-contrib-clean --save-dev
-  npm install grunt-contrib-copy --save-dev
-  npm install grunt-replace --save-dev
-  npm install grunt-contrib-uglify --save-dev
-  npm install grunt-contrib-imagemin --save-dev
-  npm install grunt-contrib-cssmin --save-dev
-  npm install grunt-contrib-htmlmin --save-dev
-  npm install grunt-contrib-compress --save-dev
-  npm install grunt-string-replace --save-dev
-  */
+    /*
+    npm install grunt-contrib-clean --save-dev
+    npm install grunt-contrib-copy --save-dev
+    npm install grunt-replace --save-dev
+    npm install grunt-contrib-uglify --save-dev
+    npm install grunt-contrib-imagemin --save-dev
+    npm install grunt-contrib-cssmin --save-dev
+    npm install grunt-contrib-htmlmin --save-dev
+    npm install grunt-contrib-compress --save-dev
+    npm install grunt-string-replace --save-dev
+    */
 };
